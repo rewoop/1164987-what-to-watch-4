d66a107fe68ab44a89bdded22e32328b6fc961a6
Serj <10rubleqow@gmail.com>
2020-06-11 23:04:10 +0300

    Добавил пакет prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {filmTitle, filmGenre, filmReleaseDate} = props;
+  const {filmTitle, filmGenre, filmReleaseDate, films} = props;
 
   return (
     <Main title={filmTitle}
       genre={filmGenre}
       releaseDate={filmReleaseDate}
+      films={films}
     />
   );
 };
 
+App.propTypes = {
+  filmTitle: PropTypes.string.isRequired,
+  filmGenre: PropTypes.string.isRequired,
+  filmReleaseDate: PropTypes.number.isRequired,
+  films: PropTypes.array.isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./components/app/app.jsx";
 const Settings = {
   FILM_TITLE: `The Rock`,
   FILM_GENRE: `Action`,
-  RELEASE_DATE: `1996`
+  RELEASE_DATE: 1996
 };
+
+const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
 
 ReactDOM.render(
     <App
       filmTitle={Settings.FILM_TITLE}
       filmGenre={Settings.FILM_GENRE}
       filmReleaseDate={Settings.RELEASE_DATE}
+      films={films}
     />,
     document.querySelector(`#root`)
 );
