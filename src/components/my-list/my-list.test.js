068f3a3b079144381b8a82c870d585e366ae8952
Sergey <10rubleqow@gmail.com>
2020-07-26 18:33:22 +0300

    fix

--- src/components/my-list/my-list.test.js
+++ src/components/my-list/my-list.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as renderer from "react-test-renderer";
-import MyList from "./my-list.jsx";
+import {MyList} from "./my-list.jsx";
 import {Provider} from "react-redux";
 import configureStore from "redux-mock-store";
 import NameSpace from "../../reducer/name-space.js";
